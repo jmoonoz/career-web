c210aa4239b4b96e361b0603ce322ed16b5cb3c5
Joel Munoz <j.munoz1@yahoo.com>
2022-06-17 22:18:06 -0700

    side menu created

--- src/components/Jobs.js
+++ src/components/Jobs.js
@@ -1,12 +1,14 @@
 import React from 'react'
+import SideMenu from './SideMenu'
 
 function Jobs() {
     return (
         <section id="Jobs">
-        <h1>this is the job section</h1>
+            <h1>this is the job section</h1>
+            <SideMenu />
 
         </section>
-        
+
     )
 }
 
